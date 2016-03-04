d889422e15cc8d287b0d3bed8c832fc317dccadc
Rohit Mathew <rohitjm@gmail.com>
2016-03-03 17:15:13 -0800

    Added button to Meals page and created MealsContainer file

---
+++ client/containers/MealsContainer.jsx
@@ -0,0 +1,26 @@
+import { connect } from 'react-redux';
+import { deleteMeal } from '../actions/index.jsx';
+import Meal from '../components/Main/Meal.jsx';
+
+/ It maps the state.user object to the
+//user prop on Main
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onRemoveClick: (meal) => {
+      dispatch(deleteMeal(mealid))
+    }
+  }
+}
+
+const MealsContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Meal)
+
+export MealsContainer;
